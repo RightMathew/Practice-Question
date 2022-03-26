3725453f58714f17848f5d44a9fb3a2dfdb028df
RightMathew <74060173+RightMathew@users.noreply.github.com>
2022-03-26 13:53:25 +0530

    Update to sum the digits of a number.c

--- Day-1/Recursion/to sum the digits of a number.c
+++ Day-1/Recursion/to sum the digits of a number.c
@@ -6,20 +6,11 @@ int addrecsingle(int n);
 int main() {
 
     int val;
-    int opt;
     
     printf("Enter the Number : ");
     scanf("%d", &val);
     
-    printf("Select One Option : \n 1.Series of Number \t2.Single Number : ");
-    scanf("%d", &opt);
-    
-    if (opt == 1)
-        printf("%d", addrec(val));
-    else if (opt == 2)
-        printf("%d", addrecsingle(val));
-    else
-        printf("Invalid Input");
+    printf("%d", addrec(val));
     
     return 0;
 }
@@ -30,12 +21,4 @@ int addrec(int n)
         return 0;
     return (n % 10 + addrec(n / 10));
     
-}
-
-int addrecsingle(int n)
-{
-    if (n == 0)
-        return n;
-    else
-        return (n + addrecsingle(n-1));
 }
