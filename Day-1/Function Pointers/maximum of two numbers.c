7ba31be373096e9f74c2d2632981385aea00d1b5
RightMathew <74060173+RightMathew@users.noreply.github.com>
2022-03-26 16:29:27 +0530

    Update maximum of two numbers.c

--- Day-1/Function Pointers/maximum of two numbers.c
+++ Day-1/Function Pointers/maximum of two numbers.c
@@ -1,30 +1,33 @@
 #include<stdio.h>
 
-int max(int a, int b);
+int max(int *a, int *b);
 
-int (*maxim)(int a, int b);
+int (*maxim)(int *a, int *b);
 
 int main()
 {
-    int a, b;
+    int a, b, *aptr, *bptr;
     
     printf("Enter the First Number : ");
     scanf("%d", &a);
     printf("Enter the Second Number : ");
     scanf("%d", &b);
     
-    maxim = max;
+    aptr = &a;
+    bptr = &b;
     
-    printf("The Maximum of two numbers %d and %d is : %d ", a,b,maxim(a, b));
+    maxim = max;
+    int large = maxim(aptr, bptr); 
+    printf("The Maximum of two numbers %d and %d is : %d ", a,b,large);
     
     return 0;
 }
 
-int max(int a, int b)
+int max(int *a, int *b)
 {
-    if (a > b)
-        return a;
+    if (*a > *b)
+        return *a;
     else
-        return b;
+        return *b;
     
 }
