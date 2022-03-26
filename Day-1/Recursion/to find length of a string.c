d5ef44be556f88e77e64e1c86058c69cef8b375c
RightMathew <74060173+RightMathew@users.noreply.github.com>
2022-03-26 16:01:25 +0530

    Update to find length of a string.c

--- Day-1/Recursion/to find length of a string.c
+++ Day-1/Recursion/to find length of a string.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
 
+int len(char str[]);
+
+int main()
+{
+    char str[50];
+    
+    printf("Enter a string : ");
+    scanf("%s", &str);
+    
+    int l = len(str);
+    printf("The length of String %s is : %d ", str, l);
+    
+    return 0;
+}
+
+int len(char str[]){
+    static int n = 0;
+    
+    if (str[n] == '\0')
+        return n;
+    else
+        n++;
+    len(str); 
+    
+}
