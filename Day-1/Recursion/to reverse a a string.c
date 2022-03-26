95f9c10f516156fac5987c4faf4a41341fdbad24
RightMathew <74060173+RightMathew@users.noreply.github.com>
2022-03-26 13:46:03 +0530

    Update to reverse a a string.c

--- Day-1/Recursion/to reverse a a string.c
+++ Day-1/Recursion/to reverse a a string.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
 
+void rev(char *a);
+
+int main()
+{
+    char str[50];
+    
+    printf("Enter the String to Reverse : ");
+    scanf("%s", &str);
+    printf("The Reversed String : ");
+    rev(str);
+    
+    return 0;
+}
+
+void rev(char *a)
+{
+    if (*a)
+    {
+        rev(a+1);
+        printf("%c", *a);
+    }
+    
+}
