837c011921c5189cd278f1e1258458a3048912d0
RightMathew <74060173+RightMathew@users.noreply.github.com>
2022-03-27 18:22:58 +0530

    Create to copy one string to another.c

---
+++ Day-1/Function Pointers/to copy one string to another.c
@@ -0,0 +1,26 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+void copy(char *ptr);
+
+void (*cp)(char*);
+
+int main() {
+
+    char str1[] = "Mathew";
+    char *ptra = str1;  
+    
+    printf("The String in ptra : %s\n", ptra);
+    
+    cp = copy;
+    cp( ptra);
+    
+    return 0;
+}
+void copy(char *ptr)
+{
+    printf("copying the content of ptra into ptrb...\n");
+    char *ptrb = ptr;
+    printf("The String in ptrb : %s", ptrb);
+    
+}
