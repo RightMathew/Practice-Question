69555c08878d0329354b367299955c83f2bce99f
RightMathew <74060173+RightMathew@users.noreply.github.com>
2022-04-02 19:16:44 +0530

    Create 2.sort.c

---
+++ Day-1/call back/2.sort.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void getdetails(void (*ptr)());
+void sort(int arr[],int n);
+
+void (*ptr)();
+
+int main() 
+{
+    ptr = sort;
+    
+    getdetails(ptr);
+    return 0;
+}
+
+void getdetails(void (*ptr)())
+{
+    int n;
+    int arr[100];
+    
+    printf("Enter the number of elements : ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter number %d : ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Elements of original array: \n");    
+    for (int i = 0; i < n; i++) {     
+        printf("%d ", arr[i]);     
+    }
+    
+    ptr(arr,n);
+}
+
+void sort(int arr[], int n)
+{
+   for (int i = 0; i < n; i++) {     
+        for (int j = i+1; j < n; j++) {     
+           if(arr[i] > arr[j]) {    
+               int temp = arr[i];    
+               arr[i] = arr[j];    
+               arr[j] = temp;    
+           }     
+        }     
+    } 
+    
+    printf("\n Elements of array sorted in ascending order: \n");    
+    for (int i = 0; i < n; i++) {     
+        printf("%d ", arr[i]);    
+    }
+}
