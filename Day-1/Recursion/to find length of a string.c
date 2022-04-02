7dd810d606188c09dc1550c005dd8792501bad76
RightMathew <74060173+RightMathew@users.noreply.github.com>
2022-04-02 13:34:55 +0530

    Update to find length of a string.c

--- Day-1/Recursion/to find length of a string.c
+++ Day-1/Recursion/to find length of a string.c
@@ -7,7 +7,7 @@ int main()
     char str[50];
     
     printf("Enter a string : ");
-    scanf("%s", &str);
+    scanf("%s", str);
     
     int l = len(str);
     printf("The length of String %s is : %d ", str, l);
